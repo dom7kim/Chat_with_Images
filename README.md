dc83c3f4e1e15ffdf8e4c1544b5fb0d68977f0c1
Dongwon Kim <born4astronomy@gmail.com>
2023-08-29 21:40:18 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,43 @@
+# LLM Image Content Understanding Chatbot
+
+An advanced chatbot that combines OpenAI's Language Models (LLM) with state-of-the-art image processing techniques. This chatbot understands the content of uploaded images, providing a platform for users to search for relevant images based on textual descriptions, generate meaningful captions, and detect objects within the images.
+
+## Features
+
+- **Image Content Understanding**: Extracts the essence of an image and presents it as a human-readable caption.
+- **Textual Image Search**: Allows users to search for images in the database by describing their content, returning images that align with the textual query.
+- **Object Detection**: Accurately identifies and locates objects within uploaded images, providing bounding box coordinates.
+- **Interactive UI**: Engage in intuitive conversations with the chatbot, guiding you through its features.
+- **Dynamic Image Database**: Users can continually enrich the image database by uploading new content.
+
+## Usage
+
+1. Engage with the chatbot through the interactive UI.
+2. Upload images to enrich the database and to leverage the chatbot's capabilities of understanding, captioning, and object detection.
+3. Search for images by describing their content, and the chatbot will retrieve the most relevant images from the database.
+
+## Setup API Keys
+
+Before using the application, you need to set up your OpenAI and HuggingFace API keys. These keys enable the chatbot to access the necessary language models and other resources.
+
+1. Obtain your OpenAI API key from [OpenAI's platform](https://beta.openai.com/signup/).
+2. Obtain your HuggingFace API token from the [HuggingFace website](https://huggingface.co/).
+
+Once you have both keys, you can set them in your environment:
+
+### For bash users (e.g., .bashrc):
+```bash
+echo 'export OPENAI_API_KEY="YOUR_OPENAI_API_KEY"' >> ~/.bashrc
+echo 'export HF_TOKEN="YOUR_HUGGINGFACE_TOKEN"' >> ~/.bashrc
+source ~/.bashrc
+```
+
+Replace `YOUR_OPENAI_API_KEY` and `YOUR_HUGGINGFACE_TOKEN` with your actual keys. Ensure you keep your keys confidential and do not commit them to public repositories.
+
+
+
+
+
+
+
+
---
+++ app.py
@@ -0,0 +1,253 @@
+import os  
+import re
+import pandas as pd
+
+import torch
+from PIL import Image
+from transformers import BlipProcessor, BlipForConditionalGeneration, DetrImageProcessor, DetrForObjectDetection
+from openai.embeddings_utils import get_embedding, cosine_similarity
+
+from langchain.agents import Tool, AgentExecutor, ConversationalChatAgent
+from langchain.memory import ConversationBufferMemory
+from langchain.chat_models import ChatOpenAI
+
+import gradio as gr
+
+df_imageDB = pd.DataFrame(columns=["file_name", "caption", "embedding", "file_path"])
+
+# ----- Define functions to be used as tools for our LLM agent ------
+
+def retrieve_image_DB(_):
+    """
+    Return the file_path, image_name, and description of the images.
+    """
+
+    return df_imageDB[['file_name', 'caption']].to_string()
+
+def check_image_file_path(inp):
+    """
+    inp (str): either the name of an image or its path.
+    """
+
+    if os.path.exists(inp) and os.path.isfile(inp):
+        file_path = inp
+    elif (df_imageDB['file_name'] == inp).any():
+        file_path = df_imageDB[df_imageDB['file_name'] == inp]['file_path'].iloc[0]  
+    else:
+        return "Image not found."
+    return file_path
+
+def display_image(inp):
+    """
+    inp (str): either the name of an image or its path.
+    """
+
+    file_path = check_image_file_path(inp)
+    
+    if file_path == "Image not found.":
+        return file_path 
+
+    return f"display {file_path}"
+    
+def get_image_caption(image_path):
+    """
+    image_path (str): the path to the image file.
+    """
+    image = Image.open(image_path).convert('RGB')
+
+    model_name = "Salesforce/blip-image-captioning-large"
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    processor = BlipProcessor.from_pretrained(model_name)
+    model = BlipForConditionalGeneration.from_pretrained(model_name).to(device)
+
+    inputs = processor(image, return_tensors='pt').to(device)
+    output = model.generate(**inputs, max_new_tokens=100)
+    caption = processor.decode(output[0], skip_special_tokens=True)
+
+    return caption
+
+def detect_objects(inp):
+    """
+    inp (str): either the name of an image or its path.
+    """
+
+    file_path = check_image_file_path(inp)
+    
+    if file_path == "Image not found.":
+        return file_path
+
+    image = Image.open(file_path).convert('RGB')
+
+    processor = DetrImageProcessor.from_pretrained("facebook/detr-resnet-50")
+    model = DetrForObjectDetection.from_pretrained("facebook/detr-resnet-50")
+
+    inputs = processor(images=image, return_tensors="pt")
+    outputs = model(**inputs)
+
+    target_sizes = torch.tensor([image.size[::-1]])
+    results = processor.post_process_object_detection(outputs, target_sizes=target_sizes, threshold=0.9)[0]
+
+    detections = ""
+    for score, label, box in zip(results["scores"], results["labels"], results["boxes"]):
+        detections += '[{}, {}, {}, {}]'.format(int(box[0]), int(box[1]), int(box[2]), int(box[3]))
+        detections += ' {}'.format(model.config.id2label[int(label)])
+        detections += ' {}\n'.format(float(score))
+
+    return detections
+
+def search_relevant_image(inp):
+    """
+    inp (str): either the name of an image or its path.
+    """
+
+    search_vector = get_embedding(inp, engine="text-embedding-ada-002")
+    new_df_imageDB = df_imageDB[['file_name', 'caption']].copy()
+    new_df_imageDB['relevance_score'] = df_imageDB['embedding'].apply(lambda x: cosine_similarity(x, search_vector))
+
+    new_df_imageDB = new_df_imageDB.sort_values("relevance_score", ascending=False)
+
+    return new_df_imageDB.to_string()
+
+
+# ------ Define tools for LLM agent. ------
+
+tools = [
+    Tool(
+        name = "Retrieve image database",
+        func=retrieve_image_DB,
+        description="Use tool to determine the path, name and caption of the images we have." 
+    ),
+    Tool(
+        name = "Display Image",
+        func=display_image,
+        description="Use this tool to display the image." \
+                    "This will display the image on the chat."              
+    ),
+    Tool(
+        name = "Detect Objects",
+        func=detect_objects,
+        description="Use this tool to detect objects in the image." \
+                  "It will return a list of all detected objects. Each element in the list in the format: " \
+                  "[x1, y1, x2, y2] class_name confidence_score."
+    ),
+    Tool(
+        name = "Search Relevant Image",
+        func=search_relevant_image,
+        description="Search for relevant image according to the input query."\
+                    "It will return the relevance scores of the images in our database"\
+                    "An input query should a word or phrase, e.g., food, playing soccer, sunny weather, riding a bike."
+    ),
+]
+
+
+# ----- Prepare our LLM chat agent with tools. ------
+
+llm = ChatOpenAI(temperature=1.0, streaming=True, model='gpt-4')
+memory = ConversationBufferMemory(
+    return_messages=True, memory_key="chat_history", output_key="output")
+chat_agent = ConversationalChatAgent.from_llm_and_tools(
+    llm=llm, 
+    tools=tools,)
+agent_executor = AgentExecutor.from_agent_and_tools(
+    agent=chat_agent,
+    tools=tools,
+    memory=memory,
+    return_intermediate_steps=True,
+    handle_parsing_errors=True,
+    verbose=True,
+)
+
+# ----- Define components for the chatbot functionality. ------
+
+def add_text(history, text):    
+    history = history + [(text,'')]
+     
+    return history, ""
+
+def generate_response(history):         
+    response = agent_executor(history[-1][0])
+    response_msg = response["output"]
+ 
+    history[-1][1] = response_msg
+
+    # Display the image if there is a sign of display in the intermediate steps.
+    if (response['intermediate_steps']!=[]):
+        for agentaction in response['intermediate_steps']:
+            if agentaction[-1].startswith('display'):
+                file_path = agentaction[-1].split(' ')[-1]
+                history = history + [((file_path,),None)]
+
+    return history, history
+
+def get_unique_file_name(file_name):
+    """
+    Return a unique file_name by appending or incrementing a number if necessary.
+    """
+    base_name, ext = os.path.splitext(file_name)
+    
+    # Extracting the existing pattern like (1), (2), etc.
+    match = re.search(r'(.*)\((\d+)\)$', base_name)
+    
+    if match:
+        base_name_without_number = match.group(1)
+        counter = int(match.group(2))
+    else:
+        base_name_without_number = base_name
+        counter = 1
+
+    new_file_name = file_name
+    
+    # Check for duplicates and modify name accordingly
+    while new_file_name in df_imageDB['file_name'].values:
+        new_file_name = f"{base_name_without_number}({counter}){ext}"
+        counter += 1
+    
+    return new_file_name
+
+def add_file(history, file):
+    global df_imageDB
+
+    file_name = get_unique_file_name(file.name.split('/')[-1])
+    file_path = file.name #file.name actually gives the path to the file, not just the file's name
+
+    caption = get_image_caption(file.name)
+    embedding = get_embedding(caption, engine='text-embedding-ada-002')
+
+    # Data to be added
+    data = {
+        "file_name": file_name,
+        "caption": "This image depicts: {}".format(caption),
+        "embedding": embedding,
+        "file_path": file_path
+    }
+
+    # Appending the data to the DataFrame
+    df_imageDB = df_imageDB.append(data, ignore_index=True)
+
+    history = history + [((file_path,), "[ {} ] - {}".format(file_name, caption))]
+    
+    return history, history
+
+
+# ----- Our chatbot begins below. ------
+
+with gr.Blocks() as demo:
+    
+    chatbot = gr.Chatbot(value=[], elem_id="chatbot", height=600)
+    with gr.Row():
+        with gr.Column(scale=0.85):
+            txt = gr.Textbox(
+                show_label=False,
+                placeholder="Enter text and press enter",
+                container=False)
+        with gr.Column(scale=0.15, min_width=0):
+            btn = gr.UploadButton("Upload 🖼️", file_types=["image"]) 
+
+    state = gr.State()
+
+    txt.submit(add_text, [chatbot, txt], [chatbot, txt], queue=False).then(
+            generate_response, inputs =[chatbot], outputs = [chatbot, state], queue=True)
+    btn.upload(add_file, [chatbot, btn], outputs = [chatbot, state], queue=False)      
+
+demo.queue()
+demo.launch()
